79ba90a2260ac26da8e38506d5b5e784d544118d
dimasna3399 <dimasna@student.telkomuniversity.ac.id>
2020-09-08 21:08:45 +0700

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,46 +2,77 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Layout from "../layouts/index"
 import Img from 'gatsby-image'
-import { useQueryParam, NumberParam, StringParam } from "use-query-params";
+import withLocation from "../layouts/withLocation"
 
-const [num, setNum] = useQueryParam("catalogUserId", NumberParam);
-
-export default () => (
-  <StaticQuery
-    query={graphql`
-      query CatalogueQuery {
-        products: allDatoCmsProduct {
-          edges {
-            node {
-              id
-              name
-              price
-              image {
-                url
-                sizes(maxWidth: 300, imgixParams: { fm: "jpg" }) {
-                  ...GatsbyDatoCmsSizes
+const CustomQueryStringComponent = ({ search }) => {
+  const { userCatalogId } = search
+  return (
+    <StaticQuery
+      query={graphql`
+        query CatalogueQuery {
+          products: allDatoCmsProduct {
+            edges {
+              node {
+                id
+                name
+                price
+                image {
+                  url
+                  sizes(maxWidth: 300, imgixParams: { fm: "jpg" }) {
+                    ...GatsbyDatoCmsSizes
+                  }
                 }
               }
             }
           }
-        }
-        site {
-          siteMetadata {
-            siteName
+          site {
+            siteMetadata {
+              siteName
+            }
           }
         }
-      }
-    `}
-render={data => {
-  const item = num !== null ? data.allDatoCmsProduct.edges.find(
-  edge => edge.node.id === num
-) : null;
-
-if (!item) {
+      `}
+  render={data => {
+    const item = userCatalogId !== null ? data.allDatoCmsProduct.edges.find(
+    edge => edge.node.id === userCatalogId
+  ) : null;
+  
+  if (!item) {
+    return <Layout site={data.site}>
+      <div className="Catalogue">
+        {
+          data.products.edges.map(({ node: product }) => (
+            <div className="Catalogue__item" key={product.id}>
+              <div
+                className="Product snipcart-add-item"
+                data-item-id={product.id}
+                data-item-price={product.price}
+                data-item-image={product.image.url}
+                data-item-name={product.name}
+                data-item-url={`/`}
+              >
+                <div className="Product__image">
+                  <Img sizes={product.image.sizes} />
+                </div> <div className="Product__details">
+                  <div className="Product__name">
+                    {product.name}
+                    <div className="Product__price">
+                      {product.price}€
+                    </div>
+                  </div>
+                  <span className="Product__buy">Buy now</span>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </Layout>
+  }
   return <Layout site={data.site}>
     <div className="Catalogue">
       {
-        data.products.edges.map(({ node: product }) => (
+        item.map(({ node: product }) => (
           <div className="Catalogue__item" key={product.id}>
             <div
               className="Product snipcart-add-item"
@@ -68,37 +99,15 @@ if (!item) {
       }
     </div>
   </Layout>
+  }}
+     />
+  )
 }
-return <Layout site={data.site}>
-  <div className="Catalogue">
-    {
-      item.map(({ node: product }) => (
-        <div className="Catalogue__item" key={product.id}>
-          <div
-            className="Product snipcart-add-item"
-            data-item-id={product.id}
-            data-item-price={product.price}
-            data-item-image={product.image.url}
-            data-item-name={product.name}
-            data-item-url={`/`}
-          >
-            <div className="Product__image">
-              <Img sizes={product.image.sizes} />
-            </div> <div className="Product__details">
-              <div className="Product__name">
-                {product.name}
-                <div className="Product__price">
-                  {product.price}€
-                </div>
-              </div>
-              <span className="Product__buy">Buy now</span>
-            </div>
-          </div>
-        </div>
-      ))
-    }
-  </div>
-</Layout>
-}}
-   />
-)
+
+CustomQueryStringComponent.propTypes = {
+  search: PropTypes.object,
+}
+
+export default withLocation(CustomQueryStringComponent)
+
+
