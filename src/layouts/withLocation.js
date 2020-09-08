9ae7770df9d58bf26ac9fe297a9e210f6a9154d2
dimasna3399 <dimasna@student.telkomuniversity.ac.id>
2020-09-09 01:29:21 +0700

    a

--- src/layouts/withLocation.js
+++ src/layouts/withLocation.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Location } from '@reach/router'
 import queryString from 'query-string'
 
-const withLocation = ComponentToWrap => props => (
+const withLocation = ComponentToWrap => props => { 
+  useEffect(()=>{
+    window.extAsyncInit = function() {
+      // the Messenger Extensions JS SDK is done loading 
+    };
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'Messenger'));
+  })
+
+  return(
+  
   <Location>
     {({ location, navigate }) => (
       <ComponentToWrap
@@ -13,6 +28,6 @@ const withLocation = ComponentToWrap => props => (
       />
     )}
   </Location>
-)
+)}
 
 export default withLocation
